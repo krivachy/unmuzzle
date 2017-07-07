9eb5e51b8167c2deadb4189baf192efd5f683373
Akos Krivachy <akos.krivachy@futurice.com>
2017-07-07 16:52:01 +0200

    :laughing:

---
+++ README.md
@@ -0,0 +1,38 @@
+# unmuzzle
+
+Do you like [Muzzle](https://muzzleapp.com/)'s notifications but wished you had such an exciting life? Well now you can!
+
+### Quickstart
+
+Just use me:
+
+```
+brew install terminal-notifier
+git clone git@github.com:krivachy/unmuzzle.git
+cd unmuzzle
+node unmuzzle.js
+```
+
+### Requirements
+
+* Some version of node. I used v7.7.1
+* Brew or if not [install terminal-notifier](https://github.com/julienXX/terminal-notifier#download)
+
+### Options
+
+You can specify the interval which the notifications should come, e.g.:
+
+```
+node unmuzzle.js 3000
+```
+
+### Known issues
+
+* The terminal icon from terminal-notifier is always visible. Don't think this can be remove. Please submit PR if you know otherwise.
+* Some notifications don't work and get displayed.
+* Actions don't work
+
+### Credits
+
+All credits go to [Muzzle](https://muzzleapp.com/) and [Bryan Jones](https://twitter.com/bdkjones) for the idea. 
+Download Muzzle and follow him on Twitter! 
---
+++ unmuzzle.js
@@ -0,0 +1,83 @@
+// Shuffle notifications
+const notifications = require('./notifications.json');
+const child_process = require('child_process');
+
+const maleNames = [
+    'Tom',
+    'Bob',
+    'Bill',
+    'John',
+    'Adam'
+];
+const femaleNames = [
+    'Eve',
+    'Mary',
+    'Wendy',
+    'Sara'
+];
+
+const interval = process.argv[2] || 5000;
+
+notifications.forEach(showNotification, this);
+
+function showNotification(notification, index) {
+    setTimeout(function () {
+        child_process.exec(buildNotificationCommand(notification))  
+    }, index * interval);
+}
+
+function buildNotificationCommand(notification) {
+    const title = replaceName(sanitizeText(notification.title));
+    const message = replaceName(sanitizeText(notification.content));
+    const icon = sanitizeIcon(notification.icon);
+    const image = notification.image
+      ? ` -contentImage ./img/${notification.image}`
+      : '';
+    const actions = notification.actions
+      ? ` -actions ${notification.actions.map(a => `'${a}'`).join(',')}`
+      : '';
+    return `terminal-notifier -message '${message}' -sound default -appIcon ./img/${icon} -title '${title}' ${image} ${actions}`;
+}
+
+function sanitizeText(text) {
+    return text.replace('&nbsp;', ' ');
+}
+
+function sanitizeIcon(icon) {
+    return icon
+      .replace('*male', 'imessage.png')
+      .replace('*female', 'imessage.png');
+}
+
+function replaceName(text) {
+    return text
+      .replace('*male', getRandomNameFrom(maleNames))
+      .replace('*female', getRandomNameFrom(femaleNames));
+}
+
+function getRandomNameFrom(array) {
+    const index = Math.floor(Math.random() * array.length)
+    return array[index];
+}
+
+
+// From: https://stackoverflow.com/a/2450976
+// Becasue I don't want dependencies and npm and stuff like that
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
